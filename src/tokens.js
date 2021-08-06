877eaf4a129afdf9e92dad1d3a2cd7c4f50777c0
Marijn Haverbeke <marijn@haverbeke.nl>
2021-08-06 15:40:40 +0200

    Get rid of calls to startOf
    By storing bracket nesting depth in the context

--- src/tokens.js
+++ src/tokens.js
@@ -2,22 +2,25 @@ import {ExternalTokenizer, ContextTracker} from "@lezer/lr"
 import {
   newline as newlineToken, eof, newlineEmpty, newlineBracketed, indent, dedent, printKeyword,
   ParenthesizedExpression, TupleExpression, ComprehensionExpression,
-  ArrayExpression, ArrayComprehensionExpression, ArgList, ParamList,
-  DictionaryExpression, DictionaryComprehensionExpression, SetExpression, SetComprehensionExpression
+  ArrayExpression, ArrayComprehensionExpression, ArgList, ParamList, importList, subscript,
+  DictionaryExpression, DictionaryComprehensionExpression, SetExpression, SetComprehensionExpression, FormatReplacement,
+  ParenL, BraceL, BracketL
 } from "./parser.terms.js"
 
 const newline = 10, carriageReturn = 13, space = 32, tab = 9, hash = 35, parenOpen = 40, dot = 46
 
 const bracketed = [
-  ParenthesizedExpression, TupleExpression, ComprehensionExpression, ArrayExpression, ArrayComprehensionExpression,
-  DictionaryExpression, DictionaryComprehensionExpression, SetExpression, SetComprehensionExpression, ArgList, ParamList
+  ParenthesizedExpression, TupleExpression, ComprehensionExpression, importList, ArgList, ParamList,
+  ArrayExpression, ArrayComprehensionExpression, subscript,
+  SetExpression, SetComprehensionExpression,
+  DictionaryExpression, DictionaryComprehensionExpression, FormatReplacement
 ]
 
 export const newlines = new ExternalTokenizer((input, stack) => {
   if (input.next < 0) {
     input.acceptToken(eof)
   } else if (input.next != newline && input.next != carriageReturn) {
-  } else if (stack.startOf(bracketed) != null) {
+  } else if (stack.context.depth < 0) {
     input.acceptToken(newlineBracketed, 1)
   } else {
     input.advance()
@@ -29,8 +32,10 @@ export const newlines = new ExternalTokenizer((input, stack) => {
 }, {contextual: true, fallback: true})
 
 export const indentation = new ExternalTokenizer((input, stack) => {
+  let cDepth = stack.context.depth
+  if (cDepth < 0) return
   let prev = input.peek(-1), depth
-  if ((prev == newline || prev == carriageReturn) && stack.startOf(bracketed) == null) {
+  if ((prev == newline || prev == carriageReturn) && stack.context.depth >= 0) {
     let depth = 0, chars = 0
     for (;;) {
       if (input.next == space) depth++
@@ -39,9 +44,9 @@ export const indentation = new ExternalTokenizer((input, stack) => {
       input.advance()
       chars++
     }
-    if (depth != stack.context.depth &&
+    if (depth != cDepth &&
         input.next != newline && input.next != carriageReturn && input.next != hash) {
-      if (depth < stack.context.depth) input.acceptToken(dedent, -chars)
+      if (depth < cDepth) input.acceptToken(dedent, -chars)
       else input.acceptToken(indent)
     }
   }
@@ -49,6 +54,7 @@ export const indentation = new ExternalTokenizer((input, stack) => {
 
 function IndentLevel(parent, depth) {
   this.parent = parent
+  // -1 means this is not an actual indent level but a set of brackets
   this.depth = depth
   this.hash = (parent ? parent.hash + parent.hash << 8 : 0) + depth + (depth << 4)
 }
@@ -57,8 +63,14 @@ const topIndent = new IndentLevel(null, 0)
 
 export const trackIndent = new ContextTracker({
   start: topIndent,
+  reduce(context, term) {
+    return context.depth < 0 && bracketed.indexOf(term) > -1 ? context.parent : context
+  },
   shift(context, term, stack, input) {
-    return term == indent ? new IndentLevel(context, stack.pos - input.pos) : term == dedent ? context.parent : context
+    if (term == indent) return new IndentLevel(context, stack.pos - input.pos)
+    if (term == dedent) return context.parent
+    if (term == ParenL || term == BracketL || term == BraceL) return new IndentLevel(context, -1)
+    return context
   },
   hash(context) { return context.hash }
 })
